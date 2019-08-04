a458be74ac3bca821b5f1f5594dd489da8daa9c2
nisiulinfosoft <48070078+nisiulinfosoft@users.noreply.github.com>
2019-08-04 12:23:48 -0500

    Proyecto DTO - Pedido

---
+++ NisiulInfoSoft/NisiulInfoSoft.DTO/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NisiulInfoSoft.DTO
+{
+    public class Pedido
+    {
+        [Key]
+        public int IdPedido { get; set; }
+        public int IdCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public int IdArticulo { get; set; }
+        public string NombreArticulo { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
